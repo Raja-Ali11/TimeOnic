34c0e9d36caeddc2d4d24c7bde15e5e1c578279a
Raja-Ali11 <m.alirajpoot2003@gmail.com>
2025-11-28 12:31:49 +0500

    Updated MongoDB Atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose")
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/TimonicDB", {
+ try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
